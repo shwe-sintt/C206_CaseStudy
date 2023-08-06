fcfb1d503ad3ac171998160c074fb6a547d6478a
22038005 <22038005@myrp.edu.sg>
2023-08-06 21:12:41 +0800

    mars updated

---
+++ C206_CaseStudy/src/Admin.java
@@ -0,0 +1,34 @@
+
+public class Admin {
+	
+	private String adminName;
+	private String adminId;
+	
+	public Admin(String adminName, String adminId) {
+		
+		this.adminName = adminName;
+		this.adminId = adminId;
+		
+	}
+	
+	public String getAdminName() {
+		
+		return adminName;
+	}
+	
+	public String setAdminName() {
+		
+		return adminName;
+	}
+	
+	public String getAdminId() {
+		
+		return adminId;
+	}
+	
+	public String setAdminId() {
+		
+		return adminId;
+	}
+
+}
