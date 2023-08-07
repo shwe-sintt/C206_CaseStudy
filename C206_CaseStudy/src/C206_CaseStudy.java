915131ab1694995a046afdc48752ba0208b566a5
shwesinthunetint <shwesinthunetint@localhost>
2023-08-07 14:55:43 +0800

    Zina- team caseStudy menus && User.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,170 +1,212 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class C206_CaseStudy {
 	
 	
-	private static final int OPTION_VIEW = 1;
-	private static final int OPTION_ADD = 2;
-	private static final int OPTION_REMOVE = 3;
-	private static final int OPTION_UPDATE = 4;
-	private static final int OPTION_QUIT = 5;
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			ArrayList<Student> studentList = new ArrayList<Student>();
-			ArrayList<Admin> adminList = new ArrayList<Admin>();
-			ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
+		ArrayList<User> userList = new ArrayList<>();
+	    userList.add(new User("admin", "admin", "admin", "Admin", "Admin"));
+	    userList.add(new User("teacher", "teacher", "teacher1", "Teacher", "Teacher"));
+	    userList.add(new User("student", "student", "student1", "Student", "Student"));
 
-			studentList.add(new Student("Rachelle Lim", "22123456", "Floorball"));
+	    Scanner scanner = new Scanner(System.in);
 
-			int option = 0;
+	    int choice = 0;
+	    while (choice != 4) {
+	        System.out.println("** School Co-Curriculum Activities (CCA) Registration System **\n");
+	        System.out.println("1. Login as Admin");
+	        System.out.println("2. Login as Teacher");
+	        System.out.println("3. Login as Student");
+	        System.out.println("4. Exit");
 
-			while (option != OPTION_QUIT) {
+	        System.out.print("Enter your choice: ");
+	        choice = scanner.nextInt();
 
-				UserRegistration.menu();
-				option = Helper.readInt("Enter an option > ");
-
-				if (option == OPTION_VIEW) {
-					// View all items
-					UserRegistration.setHeader("VIEW ALL STUDENTS");
-					UserRegistration.viewAllStudent(studentList);
-
-				} else if (option == OPTION_ADD) {
-					// Add a new item
-					UserRegistration.setHeader("ADD NEW STUDENT");			
-					addStudent(studentList);
-
-				} else if (option == OPTION_REMOVE) {
-					// Loan item
-					UserRegistration.setHeader("REMOVE STUDENT");				
-					removeStudent(studentList);
-					
-				} else if (option == OPTION_UPDATE) {
-					// Return item
-					UserRegistration.setHeader("RETURN");				
-					//studentFound = updateStudent(studentList);
-
-				} else if (option == OPTION_QUIT) {
-					System.out.println("Bye!");
-				} else {
-					System.out.println("Invalid option");
-				}
-
-			}
-
-		}
-
-		public static void menu() {
-			UserRegistration.setHeader("CCA REGISTRATION APP");
-			System.out.println("1. View all student");
-			System.out.println("2. Add new student");
-			System.out.println("3. Remove existing student");
-			System.out.println("4. Update existing student");
-			System.out.println("5. Quit");
-			Helper.line(80, "-");
-
-		}
-		
-		public static void setHeader(String header) {
-			Helper.line(80, "-");
-			System.out.println(header);
-			Helper.line(80, "-");
-		}
-		
-		//================================= Option 1 View (CRUD - Read) =================================
-		public static void viewAllStudent(ArrayList<Student> studentList) {
-			
-			String output = String.format("%-20s %-20s %-20s\n", "Student Name", "Student ID",
-					"Chosen Activity");
-
-			for (int i = 0; i < studentList.size(); i++) {
-
-				if (studentList.get(i) != null) {
-
-					String studentName = studentList.get(i).getStudentName();
-					String studentId = studentList.get(i).getStudentId();
-					String activityChosen = studentList.get(i).getactivityChosen();
-
-					output += String.format("\n%-20s %-20s %-20s", studentName, studentId, activityChosen);
-
-				}
-			}
-			System.out.println(output);
-			// ------------------- END OF CODE
-
-		}
-		
-		//================================= Option 2 Add (CRUD - Create)=================================
-		public static void addStudent(ArrayList<Student> studentList) {
-
-			String newStuName = Helper.readString("Enter student name > ");
-			String newStuID = Helper.readString("Enter student ID > ");
-			String newStuChosenAct = Helper.readString("Enter chosen activity > ");
-
-			Student newStudent = new Student(newStuName, newStuID, newStuChosenAct);
-			studentList.add(newStudent);
-
-			System.out.println("Student added successfully!");
-		}
-		
-		//================================= Option 3 Remove (CRUD - Delete)=================================
-		public static boolean removeStudent(ArrayList<Student> studentList) {
-
-			String removeStuID = Helper.readString("Enter student ID to remove > ");
-
-			boolean studentFound = false;
-			for (int i = 0; i < studentList.size(); i++) {
-
-				if (studentList.get(i).getStudentId().equals(removeStuID)) {
-
-					studentList.remove(i);
-					studentFound = true;
-					break;
-				}
-			}
-
-			if (studentFound) {
-				System.out.println("Student removed successfully!");
-			} else {
-				System.out.println("Student not found!");
-			}
-
-			return studentFound;
-		}
-		
-		
-		//================================= Option 4 Update (CRUD - Update)=================================
-		public static boolean updateStudent(ArrayList<Student> studentList) {
-			
-			String updStuID = Helper.readString("Enter student ID to update > ");
-
-			boolean studentFound = false;
-			for (int i = 0; i < studentList.size(); i++) {
-
-				if (studentList.get(i).getStudentId().equals(updStuID)) {
-
-					String updStuName = Helper.readString("Enter student name to update > ");
-
-					String updStuChosenAct = Helper.readString("Enter chosen activity to update > ");
-
-					studentList.get(i).setStudentName(updStuName);
-					studentList.get(i).setactivityChosen(updStuChosenAct);
-
-					studentFound = true;
-					break;
-				}
-			}
-
-			if (studentFound) {
-				System.out.println("Student updated successfully!");
-			} else {
-				System.out.println("Student not found!");
-			}
-
-			return studentFound;
-		}
-
-
-
+	        switch (choice) {
+	            case 1:
+	                loginAdmin(userList);
+	                break;
+	            case 2:
+	                loginTeacher(userList);
+	                break;
+	            case 3:
+	                loginStudent(userList);
+	                break;
+	            case 4:
+	                System.out.println("Exiting the system...");
+	                break;
+	            default:
+	                System.out.println("Invalid choice!");
+	        }
+	        
+	    }
+	    
 	}
+	
+	private static void loginAdmin(ArrayList<User> userList) {
+		Scanner scanner = new Scanner(System.in);
+
+        String username, password;
+
+        System.out.print("Enter your username: ");
+        username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        password = scanner.nextLine();
+
+        for (User user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password) && user.getRole().equalsIgnoreCase("admin")) {
+                System.out.println("\nWelcome, Admin!\n");
+                adminMenu();
+                return;
+            }
+        }
+        
+        System.out.println("Invalid username, password, or role!");
+    }
+
+    private static void loginTeacher(ArrayList<User> userList) {
+    	Scanner scanner = new Scanner(System.in);
+
+        String username, password;
+
+        System.out.print("Enter your username: ");
+        username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        password = scanner.nextLine();
+
+        for (User user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password) && user.getRole().equalsIgnoreCase("teacher")) {
+                System.out.println("\nWelcome, Teacher!\n");
+                teacherMenu();
+                return;
+            }
+        }
+        
+        System.out.println("Invalid username, password, or role!");
+    	
+    }
+
+    private static void loginStudent(ArrayList<User> userList) {
+    	Scanner scanner = new Scanner(System.in);
+
+        String username, password;
+
+        System.out.print("Enter your username: ");
+        username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        password = scanner.nextLine();
+
+        for (User user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password) && user.getRole().equalsIgnoreCase("student")) {
+                System.out.println("\nWelcome, Student!\n");
+                studentMenu();
+                return;
+            }
+        }
+        
+        System.out.println("Invalid username, password, or role!");
+        
+    }
+    
+    private static void adminMenu() {
+		Scanner scanner = new Scanner(System.in);
+
+		int choice = 0;
+		while (choice != 7) {
+			System.out.println("** Admin Menu **\n");
+			System.out.println("1. Add a new student");
+			System.out.println("2. Update a student's information");
+			System.out.println("3. Delete a student");
+			System.out.println("4. View all students");
+			System.out.println("5. Log out");
+
+			System.out.print("Enter your choice: ");
+			choice = scanner.nextInt();
+
+			switch (choice) {
+			case 1:
+				//addStudent();
+				break;
+			case 2:
+				// updateStudent();
+				break;
+			case 3:
+				// deleteStudent();
+				break;
+			case 4:
+				// viewAllStudents();
+				break;
+			case 5:
+				System.out.println("Logging out...");
+
+			}
+		}
+	}
+
+	private static void teacherMenu() {
+		Scanner scanner = new Scanner(System.in);
+
+		int choice = 0;
+		while (choice != 7) {
+			System.out.println("** Teacher Menu **\n");
+			System.out.println("1. Add a new activity");
+			System.out.println("3. Delete an activity");
+			System.out.println("4. View all activity");
+			System.out.println("5. Log out");
+
+			System.out.print("Enter your choice: ");
+			choice = scanner.nextInt();
+
+			switch (choice) {
+			case 1:
+				// addStudent();
+				break;
+			case 2:
+				// deleteStudent();
+				break;
+			case 3:
+				// viewAllStudents();
+				break;
+			case 4:
+				System.out.println("Logging out...");
+			}
+			
+		}
+	}
+	
+	private static void studentMenu() {
+		Scanner scanner = new Scanner(System.in);
+
+		int choice = 0;
+		while (choice != 7) {
+			System.out.println("** Student Menu **\n");
+			System.out.println("1. Apply for an activity");
+			System.out.println("2. View all activity");
+			System.out.println("3. Log out");
+
+			System.out.print("Enter your choice: ");
+			choice = scanner.nextInt();
+
+			switch (choice) {
+			case 1:
+				// addStudent();
+				break;
+			case 2:
+				// deleteStudent();
+				break;
+			case 3:
+				// viewAllStudents();
+				break;
+			case 4:
+				System.out.println("Logging out...");
+			}
+			
+		}
+	}
+}
