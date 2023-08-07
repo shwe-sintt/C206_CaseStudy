a2093a69b95ed493758e8ba6423626d2ab0cb53b
22003619 <22003619@myrp.edu.sg>
2023-08-07 10:34:46 +0800

    #

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		ArrayList<Admin> adminList = new ArrayList<Admin>();
+		ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
 
 	}
 
--- C206_CaseStudy/src/Teacher.java
+++ C206_CaseStudy/src/Teacher.java
@@ -1,18 +1,3 @@
-/*
- * I declare that this code was written by me. 
- * I do not copy or allow others to copy my code. 
- * I understand that copying code is considered as plagiarism.
- * 
- * Student Name: 22003619
- * Student ID: {type your id here}
- * Class: {type your class here}
- * Date/Time created: Monday 07-08-2023 10:25
- */
-
-/**
- * @author 22003619
- *
- */
 public class Teacher {
 
 }
