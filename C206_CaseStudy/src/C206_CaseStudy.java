d61f5f35550abe9871233498c4724d4d2f49acaa
22038005 <22038005@myrp.edu.sg>
2023-08-10 13:50:36 +0800

    mars updated

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,7 +7,8 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ArrayList<User> userList = new ArrayList<>();
+		ArrayList<User> userList = new ArrayList<User>();
+		
 	    userList.add(new User("admin", "admin", "admin", "Admin", "Admin"));
 	    userList.add(new User("teacher", "teacher", "teacher1", "Teacher", "Teacher"));
 	    userList.add(new User("student", "student", "student1", "Student", "Student"));
@@ -114,33 +115,32 @@ public class C206_CaseStudy {
         
     }
     
-    private static void adminMenu() {
-		Scanner scanner = new Scanner(System.in);
+    public static void adminMenu() {
+		Scanner scanner = new Scanner(System.in); 
 
 		int choice = 0;
-		while (choice != 5) {
+		while (choice != 4) {
 			System.out.println("** Admin Menu **\n");
-			System.out.println("1. Add a new student");
-			System.out.println("2. Update a student's information");
-			System.out.println("3. Delete a student");
-			System.out.println("4. View all students");
-			System.out.println("5. Log out");
+			System.out.println("1. Manage User Account");
+			System.out.println("2. Manage User Activities");
+			System.out.println("3. Manage User Registration");
+			System.out.println("4. Log out");
 
 			System.out.print("Enter your choice: ");
 			choice = scanner.nextInt();
 
 			switch (choice) {
 			case 1:
-				//addStudent();
+				manageUserAccounts();
 				break;
 			case 2:
-				// updateStudent();
+				//
 				break;
 			case 3:
-				// deleteStudent();
+				//
 				break;
 			case 4:
-				// viewAllStudents();
+				//
 				break;
 			case 5:
 				System.out.println("Logging out...");
@@ -148,6 +148,48 @@ public class C206_CaseStudy {
 			}
 		}
 	}
+    
+    public static void manageUserAccounts() {
+        Scanner scanner = new Scanner(System.in); 
+
+		ArrayList<User> userList = new ArrayList<User>();
+		
+	    userList.add(new User("22123456", "Amelia Row", "Amelia", " ", "Student", "Hockey"));
+	    userList.add(new User("22654321", "Rachelle Lim", "Rachelle", " ", "Student", "Floorball"));
+	    
+        int choice = 0;
+        while (choice != 5) {
+            System.out.println("\n** Manage User Accounts **\n");
+            System.out.println("1. View all students");
+            System.out.println("2. Add a new student");
+            System.out.println("3. Delete a student");
+            System.out.println("4. Back");
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+//                	viewAllStudent(userList);
+                	C206_CaseStudy.viewAllUser(userList);
+                    break;
+                case 2:
+//                	addUserAccount(userList);
+                	User stu = inputUser();
+					C206_CaseStudy.addUser(userList, stu);
+					System.out.println("Student added successfully!");
+                    break;
+                case 3:
+                	removeUser(userList);
+                    break;
+                case 4:
+                    System.out.println("Going back to the main menu...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 
     private static void teacherMenu() {
         Scanner scanner = new Scanner(System.in);
@@ -306,9 +348,96 @@ public class C206_CaseStudy {
 				break;
 			case 4:
 				System.out.println("Logging out...");
-				break;
 			}
 			
 		}
 	}
+	
+	//========================================================================================== Admin ============================================================================
+	
+	//================================= Option 1 View (CRUD - Read) =================================
+	public static String retrieveAllUser(ArrayList<User> userList) {
+		String output = "";
+
+		for (int i = 0; i < userList.size(); i++) {
+
+			output += String.format("%-60s\n", userList.get(i).toString());
+		}
+		return output;
+	}
+	
+	public static void viewAllUser(ArrayList<User> userList) {
+		String output = String.format("%-20s %-20s %-20s\n\n", "Student Name", "Student ID",
+				"Chosen Activity");
+		 output += retrieveAllUser(userList);	
+		System.out.println(output);
+	}
+	
+	//================================= Option 2 Add (CRUD - Create)=================================
+	public static User inputUser() {
+		String newUserName = Helper.readString("Enter student name > ");
+		String newUserId = Helper.readString("Enter student ID > ");
+		String newUserChosenAct = Helper.readString("Enter chosen activity > ");
+		String newUseruName = "";
+		String newUserPass = "";
+		String newUserRole = "Student";
+
+
+		User stu = new User(newUserId, newUserName, newUseruName, newUserPass, newUserRole, newUserChosenAct);
+		return stu;
+		
+	}
+	
+	public static void addUser(ArrayList<User> userList, User stu) {
+		User student;
+		for(int i = 0; i < userList.size(); i++) {
+			student = userList.get(i);
+			if (student.getName().equalsIgnoreCase(stu.getName()) )
+				return;
+		}
+		if ((stu.getName().isEmpty()) || (stu.getId().isEmpty()) || (stu.getActivityChosen().isEmpty())) {
+			return;
+		}
+		userList.add(stu);
+		
+	}
+	
+	//================================= Option 3 Remove (CRUD - Delete)=================================
+	public static boolean removeUser(ArrayList<User> userList) {
+
+		String removeStuID = Helper.readString("Enter student ID to remove > ");
+
+		boolean studentFound = false;
+		for (int i = 0; i < userList.size(); i++) {
+
+			if (userList.get(i).getId().equals(removeStuID)) {
+
+				userList.remove(i);
+				studentFound = true;
+				break;
+			}
+		}
+
+		if (studentFound) {
+			System.out.println("Student removed successfully!");
+		} else {
+			System.out.println("Student not found!");
+		}
+
+		return studentFound;
+	}
+	
+
+	
+	//========================================================================================== Teacher ============================================================================
+	
+	
+	
+	
+	
+	
+	
+	//========================================================================================== Student ============================================================================
+	
+	
 }
