7484872e91af6ee79dc565650ce37932469a56a8
22038005 <22038005@myrp.edu.sg>
2023-08-11 14:56:44 +0800

    mars updated

--- C206_CaseStudy/src/Activities.java
+++ C206_CaseStudy/src/Activities.java
@@ -1,49 +1,57 @@
 
 
-public class Activities {
+public class Activities{
 	
-		private String id;
-		private String name;
-		private String description;
-		private String criteria;
+		private String activityId;
+		private String activityName;
+		private String activityDescription;
+		private String activityCriteria;
 
-		public Activities(String id,String name, String description, String criteria) {
-			this.id = id;
-			this.name = name;
-			this.description = description;
-			this.criteria = criteria;
+//		public Activities(String id,String name, String description, String criteria) {
+//			this.id = id;
+//			this.name = name;
+//			this.description = description;
+//			this.criteria = criteria;
+//		}
+		
+		public Activities(String activityId,String activityName, String activityDescription, String activityCriteria) {
+//			super(id, activityId, activityName, activityDescription, activityCriteria);
+			this.activityId = activityId;
+			this.activityName = activityName;
+			this.activityDescription = activityDescription;
+			this.activityCriteria = activityCriteria;
 		}
 
-		public String getId() {
-			return id;
+		public String getActivityId() {
+			return activityId;
 		}
 
-		public void setId(String id) {
-			this.id = id;
+		public void setActivityId(String activityId) {
+			this.activityId = activityId;
 		}
 
-		public String getName() {
-			return name;
+		public String getActivityName() {
+			return activityName;
 		}
 
-		public void setName(String name) {
-			this.name = name;
+		public void setActivityName(String activityName) {
+			this.activityName = activityName;
 		}
 
-		public String getDescription() {
-			return description;
+		public String getActivityDescription() {
+			return activityDescription;
 		}
 
-		public void setDescription(String description) {
-			this.description = description;
+		public void setActivityDescription(String activityDescription) {
+			this.activityDescription = activityDescription;
 		}
 
-		public String getCriteria() {
-			return criteria;
+		public String getActivityCriteria() {
+			return activityCriteria;
 		}
 
-		public void setCriteria(String criteria) {
-			this.criteria = criteria;
+		public void setActivityCriteria(String activityCriteria) {
+			this.activityCriteria = activityCriteria;
 		}
 	
 }
